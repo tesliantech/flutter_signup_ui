0e37e8201c02ab0857238e08ec9e7870d95add02
teslian tech <86150422+tesliantech@users.noreply.github.com>
2024-10-09 15:02:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+function a0_0x1345() { let t = ["maxWidth", "9Mjgqic", "message", "right", "pointer", "overflow", "\n#chatbase-bubble-button,\n#chatbase-bubble-button * {\n margin: 0;\n padding: 0;\n box-sizing: border-box;\n}\nhtml.", "left", "display_name", "/chatbot-iframe/", "0.75rem", "scale(1)", "backgroundColor", "5000vdZZEn", "black", "setAttribute", "innerHTML", "top", "localhost", "scrolling", "button", "100%", "json", "auto_open_chat_window_after", "yes", "chatbase-bubble-window", "?theme=dark", '<img src="https://backend.chatbase.co/storage/v1/object/public/chat-icons/', "none", "ariaLabel", "3183392CZyWdI", "opacity 0.5s ease, transform 0.5s ease", "stopPropagation", "get", "abs", "sans-serif", "position", "Chatbot", "appendChild", "#000000", "parseInt", "chatbotId", "button_color", "theme", "embeddedChatbotConfig", "2892GIXDbm", "querySelector", "100dvh", "textAlign", "margin", "3340995eDRqyN", "getAttribute", "click", "85px", "rgba(150, 150, 150, 0.2) 0px 10px 30px 0px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px", "complete", "chatbase-message-bubbles", "title", "border", "chatbase.fyi", "true", "innerWidth", "https://", "flexDirection", "html", "-7px", "searchParams", "1DMxyPs", '<svg width="55" height="55" viewBox="0 0 1120 1120" fill="none" xmlns="http://www.w3.org/2000/svg">\n    <path fill-rule="evenodd" clip-rule="evenodd" d="M252 434C252 372.144 302.144 322 364 322H770C831.856 322 882 372.144 882 434V614.459L804.595 585.816C802.551 585.06 800.94 583.449 800.184 581.405L763.003 480.924C760.597 474.424 751.403 474.424 748.997 480.924L711.816 581.405C711.06 583.449 709.449 585.06 707.405 585.816L606.924 622.997C600.424 625.403 600.424 634.597 606.924 637.003L707.405 674.184C709.449 674.94 711.06 676.551 711.816 678.595L740.459 756H629.927C629.648 756.476 629.337 756.945 628.993 757.404L578.197 825.082C572.597 832.543 561.403 832.543 555.803 825.082L505.007 757.404C504.663 756.945 504.352 756.476 504.073 756H364C302.144 756 252 705.856 252 644V434ZM633.501 471.462C632.299 468.212 627.701 468.212 626.499 471.462L619.252 491.046C618.874 492.068 618.068 492.874 617.046 493.252L597.462 500.499C594.212 501.701 594.212 506.299 597.462 507.501L617.046 514.748C618.068 515.126 618.874 515.932 619.252 516.954L626.499 536.538C627.701 539.788 632.299 539.788 633.501 536.538L640.748 516.954C641.126 515.932 641.932 515.126 642.954 514.748L662.538 507.501C665.788 506.299 665.788 501.701 662.538 500.499L642.954 493.252C641.932 492.874 641.126 492.068 640.748 491.046L633.501 471.462Z" fill="', "unset", "14px", "body", "space-between", "height", "1433010XimlfP", "flex", "chat-button", "remove", ",\nhtml.", "__cb_widget_active", "fontFamily", "1rem", "classList", "45166IEWBwB", "22px", "bottom", "http://localhost:3000", "style", "transform", '" \n    style="width:30px;height:30px;"\n>\n    <g>\n        <rect x="11" y="1" width="2" height="5" opacity=".14" />\n        <rect\n            x="11"\n            y="1"\n            width="1"\n            height="5"\n            transform="rotate(30 12 12)"\n            opacity=".29"\n        />\n        <rect\n            x="11"\n            y="1"\n            width="1"\n            height="5"\n            transform="rotate(60 12 12)"\n            opacity=".43"\n        />\n        <rect\n            x="11"\n            y="1"\n            width="1"\n            height="5"\n            transform="rotate(90 12 12)"\n            opacity=".57"\n        />\n        <rect\n            x="11"\n            y="1"\n            width="1"\n            height="5"\n            transform="rotate(120 12 12)"\n            opacity=".71"\n        />\n        <rect\n            x="11"\n            y="1"\n            width="1"\n            height="5"\n            transform="rotate(150 12 12)"\n            opacity=".86"\n        />\n        <rect\n            x="11"\n            y="1"\n            width="1"\n            height="5"\n            transform="rotate(180 12 12)"\n        />\n        <animateTransform\n            attributeName="transform"\n            type="rotate"\n            calcMode="discrete"\n            dur="0.75s"\n            values="0 12 12;30 12 12;60 12 12;90 12 12;120 12 12;150 12 12;180 12 12;210 12 12;240 12 12;270 12 12;300 12 12;330 12 12;360 12 12"\n            repeatCount="indefinite"\n        />\n    </g>\n</svg>', "21472100fyzYdz", "0 4px 8px 0 rgba(0, 0, 0, 0.2)", "scale(1.08)", "chat_icon", "scale(0.9)", '" width="24" height="24">\n    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />\n  </svg>\n  ', "all .2s ease-in-out", "824px", "fixed", "cursor", "padStart", "type", "color", "beforebegin", "getElementsByTagName", "85dvh", "includes", "width", "#FFFFFF", "chatbase-bubble-button", "center", "&#10005;", "toString", "onload", "dark", "bold", "https://qa.chatbase.co", "GET", "substr", "opacity", "origin", "setItem", "script", "zIndex", "postMessage", "fontSize", "addEventListener", "length", "maxHeight", "16px", "block", "contentWindow", "alignItems", "flex-start", "charAt", "column", "application/json", "justifyContent", "display", "getItem", "rgba(150, 150, 150, 0.15) 0px 6px 24px 0px, rgba(150, 150, 150, 0.15) 0px 0px 0px 1px", '<svg\n    viewBox="0 0 30 30"\n    xmlns="http://www.w3.org/2000/svg"\n    stroke="', "text/javascript", "12px", "2642956RAdOwV", "white", "resize", "data", "transition", "chatbot icon", "/api/get-chatbot-styles/", "embedSuccess", "458px", "insertAdjacentElement", "mouseleave", "mouseenter", "padding", "gap", "message_bubbles_have_been_shown", "div", "boxShadow", "borderRadius", "round", '\n  <svg id="closeIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.3" stroke="', "createElement", "align_chat_button", "src", "close", "onerror", "fontWeight", "chatbaseConfig", "iframe", '" alt="', "5rem", "https://www.chatbase.co", "10px", "absolute", "min"]; return (a0_0x1345 = function() { return t })() }
+
+function a0_0x2f8a(t, e) { let n = a0_0x1345(); return (a0_0x2f8a = function(t, e) { return t -= 115, n[t] })(t, e) }(() => {
+    for (var t = a0_0x2f8a, e = a0_0x1345();;) try {
+        if (370991 == +parseInt(t(123)) * (parseInt(t(139)) / 2) + parseInt(t(130)) / 3 + parseInt(t(200)) / 4 + parseInt(t(247)) / 5 * (parseInt(t(279)) / 6) + parseInt(t(284)) / 7 + parseInt(t(264)) / 8 + parseInt(t(235)) / 9 * (-parseInt(t(146)) / 10)) break;
+        e.push(e.shift())
+    } catch (t) { e.push(e.shift()) }
+})(), (() => {
+    let t = a0_0x2f8a,
+        I = document.currentScript;
+    async function e() {
+        let l = a0_0x2f8a;
+        if (window[l(226)] ? .[l(207)]) return;
+        let n = l(248),
+            t = new URL(I[l(222)]),
+            a = l(135),
+            o = l(230),
+            e = ((t[l(176)] === l(142) || t.origin === l(172) || t[l(176)].endsWith(l(115)) || t[l(176)][l(162)]("localhost")) && (o = t[l(176)]), null);
+        try { e = t[l(122)][l(267)]("chatbotId") } catch (t) { console.log(t) }
+        let i = I.getAttribute(l(275)) || window[l(278)] ? .[l(275)] || I.id || window[l(226)] ? .chatbotId || e,
+            r = I[l(285)]("domain") || window.embeddedChatbotConfig ? .domain,
+            s = r ? r[l(162)](l(252)) ? "http://" + r : l(118) + r : t[l(176)],
+            d = document[l(220)](l(143)),
+            c = (d[l(250)] = l(240) + a + l(134) + a + " body {\n  position: fixed !important;\n  top: 0;\n  left: 0;\n  overscroll-behavior: none;\n  width: 0 !important;\n  height: 0 !important;\n  margin: 0 !important;\n  overflow: hidden !important;\n  overflow-y: hidden !important;\n  overflow-x: hidden !important;\n  transform: none !important;\n}\n", document[l(127)].appendChild(d), "white"),
+            h = null,
+            p = !1,
+            w = l(237),
+            u = document.createElement(l(254)),
+            b = (u[l(157)] = l(254), u[l(249)]("id", l(165)), u[l(143)][l(270)] = l(154), u.style[l(292)] = "0", u.style[l(141)] = l(137), u.style.right = l(137), u[l(143)].width = "55px", u[l(143)][l(129)] = "55px", u[l(143)][l(217)] = "27.5px", u[l(143)][l(246)] = n, u[l(143)][l(216)] = l(147), u.style.cursor = l(238), u[l(143)][l(179)] = 2147483645, u[l(143)][l(204)] = l(152), u[l(263)] = l(132), document[l(220)]("div")),
+            m = (b[l(249)]("id", l(290)), b[l(143)][l(270)] = "fixed", b.style[l(141)] = "85px", b[l(143)][l(217)] = l(231), b.style[l(136)] = l(269), b.style[l(181)] = l(185), b[l(143)].zIndex = 2147483644, b[l(143)][l(155)] = l(238), b[l(143)][l(119)] = l(191), b[l(143)][l(213)] = "50px", b[l(143)][l(234)] = "70vw", b[l(143)][l(194)] = "none", document[l(220)](l(215))),
+            y = (m[l(250)] = l(167), m[l(143)].position = l(232), m[l(143)][l(251)] = l(121), m.style[l(237)] = "-7px", m[l(143)][l(225)] = l(171), m[l(143)][l(194)] = l(262), m[l(143)][l(193)] = l(166), m[l(143)][l(188)] = "center", m[l(143)][l(179)] = 2147483643, m.style[l(163)] = l(140), m[l(143)][l(129)] = "22px", m.style.borderRadius = "50%", m.style[l(282)] = "center", m[l(143)][l(181)] = l(199), m[l(143)][l(155)] = l(238), b[l(272)](m), document[l(127)].appendChild(b), u[l(182)](l(211), t => {
+                var e = l;
+                u[e(143)][e(144)] = e(148)
+            }), u.addEventListener(l(210), t => {
+                var e = l;
+                u[e(143)][e(144)] = e(245)
+            }), document[l(220)]("div")),
+            x = (y.style[l(194)] = "flex", y[l(143)][l(188)] = l(166), y.style[l(193)] = l(166), y[l(143)][l(163)] = l(255), y[l(143)][l(129)] = l(255), y[l(143)][l(179)] = 2147483646, u[l(272)](y), u[l(182)](l(286), () => { var t = l; "none" === f[t(143)].display ? (p = !0, b.style[t(194)] = t(262), window[t(117)] < 640 && document.querySelector(t(120)) ? .[t(138)].add(a), v(), y[t(250)] = _()) : (f[t(143)].display = t(262), y[t(250)] = C()) }), window.addEventListener(l(236), t => { var e = l; "iframeReady" === t[e(203)][e(157)] && x[e(187)].postMessage({ windowInnerWidth: window[e(117)] }, "*"), t.origin === s && t[e(203)][e(157)] === e(223) && (f[e(143)][e(194)] = e(262), document[e(280)](e(120)) ? .classList[e(133)](a), y[e(250)] = C()) }), b[l(182)]("click", () => {
+                var t = l;
+                p = !0, b[t(143)].display = t(262), v(), y.innerHTML = _()
+            }), document[l(220)](l(227))),
+            g = (x[l(143)][l(129)] = l(255), x.style[l(163)] = l(255), x[l(143)][l(292)] = "none", x[l(143)][l(194)] = "none", document[l(220)](l(215))),
+            f = (g[l(143)][l(194)] = "flex", g[l(143)][l(193)] = l(166), g.style.height = "100%", g[l(143)][l(188)] = l(166), document[l(220)](l(215))),
+            v = (f[l(253)] = l(258), setTimeout(() => {
+                var t = l;
+                f[t(253)] = t(258)
+            }, 3e3), f[l(249)]("id", l(259)), f.style[l(292)] = l(262), f[l(143)][l(270)] = l(154), f[l(143)][l(119)] = l(191), f[l(143)][l(193)] = l(128), f.style[l(216)] = l(288), f[l(143)][l(141)] = window[l(117)] < 640 ? "0" : l(287), f[l(143)][l(237)] = window.innerWidth < 640 ? "0" : "1rem", f[l(143)].width = window[l(117)] < 640 ? "100%" : l(208), f[l(143)][l(129)] = window[l(117)] < 640 ? l(281) : l(161), f.style[l(184)] = window[l(117)] < 640 ? "" : l(153), f[l(143)][l(217)] = window.innerWidth < 640 ? "0" : "0.75rem", f[l(143)][l(194)] = l(262), f[l(143)][l(179)] = 2147483646, f[l(143)][l(239)] = "hidden", f[l(272)](g), f.appendChild(x), document[l(127)][l(272)](f), window.addEventListener(l(202), () => {
+                var t = l;
+                w === t(237) ? (f[t(143)][t(237)] = window[t(117)] < 640 ? 0 : t(137), f.style[t(241)] = "unset") : (f[t(143)][t(241)] = window[t(117)] < 640 ? 0 : t(137), f[t(143)][t(237)] = t(125)), f[t(143)][t(141)] = window[t(117)] < 640 ? 0 : t(229), f.style.top = window.innerWidth < 640 ? 0 : "unset", f[t(143)].width = window.innerWidth < 640 ? t(255) : "448px", f[t(143)][t(129)] = window[t(117)] < 640 ? t(281) : t(161), f.style.borderRadius = window[t(117)] < 640 ? "0" : t(244), f.style[t(194)] !== t(262) && (window[t(117)] < 640 ? document.body[t(138)].add(a) : document[t(127)][t(138)][t(133)](a)), x[t(187)][t(180)]({ windowInnerWidth: window.innerWidth }, "*")
+            }), async() => {
+                var t = l;
+                x.src || (x[t(222)] = s + t(243) + i + t(260)), f[t(143)][t(194)] = t(131)
+            });
+
+        function C() { var t = l(124) + c + '"/>\n    <path d="M771.545 755.99C832.175 755.17 881.17 706.175 881.99 645.545L804.595 674.184C802.551 674.94 800.94 676.551 800.184 678.595L771.545 755.99Z" fill="' + c + '"/>\n    </svg>            \n      '; return h || t }
+
+        function _() { var t = l; return t(219) + c + t(151) }
+
+        function L(t, e) {
+            let n = l,
+                a = t;
+            "#" === a[n(190)](0) && (a = a[n(174)](1));
+            var t = t => { var e = n; return Math[e(233)](255, Math.max(0, t)) },
+                o = Number[n(274)](a[n(174)](0, 2), 16),
+                i = Number[n(274)](a.substr(2, 2), 16),
+                r = Number[n(274)](a.substr(4, 2), 16),
+                e = .5 < (.299 * o + .587 * i + .114 * r) / 255 ? -1 * Math[n(268)](e) : Math[n(268)](e),
+                o = t(o + Math[n(218)](255 * e)),
+                i = t(i + Math[n(218)](255 * e)),
+                t = t(r + Math[n(218)](255 * e));
+            return "#" + o[n(168)](16).padStart(2, "0") + i[n(168)](16)[n(156)](2, "0") + t[n(168)](16)[n(156)](2, "0")
+        }
+        x[l(169)] = () => {
+            var t = l;
+            x[t(143)][t(194)] = t(186), g[t(143)][t(194)] = t(262)
+        }, (async() => {
+            let r = l,
+                t = await fetch(o + r(206) + i, { method: r(173), headers: { "Content-Type": r(192) } }),
+                { styles: s, initialMessages: d } = await t[r(256)]();
+            x[r(291)] = s ? .[r(242)] || r(271), f[r(143)][r(246)] = s[r(277)] === r(170) ? r(248) : r(201), u[r(143)].backgroundColor = s[r(276)] || n, s[r(221)] === r(241) ? (w = "left", u[r(143)][r(241)] = r(137), u.style[r(237)] = "unset", b[r(143)].left = r(137), b.style[r(237)] = "unset", f[r(143)][r(241)] = window[r(117)] < 640 ? 0 : r(137), f.style[r(237)] = r(125)) : (u.style[r(237)] = r(137), u.style[r(241)] = r(125), b.style[r(237)] = r(137), b.style[r(241)] = "unset", f[r(143)][r(237)] = window[r(117)] < 640 ? 0 : "1rem", f[r(143)][r(241)] = "unset"), f[r(209)](r(159), u), s[r(149)] && (h = r(261) + s[r(149)] + r(228) + (s.display_name ? ? r(205)) + "\" style='width: 55px; height: 55px; border-radius: 50%;' />");
+            var e = (t => {
+                    let e = l,
+                        n = t;
+                    "#" === n[e(190)](0) && (n = n[e(174)](1));
+                    var t = Number[e(274)](n.substr(0, 2), 16),
+                        a = Number.parseInt(n.substr(2, 2), 16),
+                        o = Number[e(274)](n[e(174)](4, 2), 16);
+                    return .5 < (t = (.299 * t + .587 * a + .114 * o) / 255) ? e(248) : e(201)
+                })(s[r(276)] || n),
+                e = (c = e, y[r(250)] = C(), d.forEach((t, e) => {
+                    let n = r,
+                        a = document[n(220)](n(215)),
+                        o = (a.style[n(194)] = n(131), a.style[n(193)] = s[n(221)] === n(241) ? n(189) : "flex-end", document[n(220)]("div"));
+                    o[n(143)][n(246)] = s[n(277)] === n(170) ? "black" : n(201), o[n(143)][n(158)] = "dark" === s[n(277)] ? n(201) : n(248), o[n(143)].boxShadow = n(288), o[n(143)].borderRadius = n(231), o[n(143)][n(212)] = "20px", o.style[n(283)] = "8px", o[n(143)][n(181)] = n(126);
+                    o[n(250)] = t;
+                    var i = o[n(160)]("a");
+                    for (let t = 0; t < i[n(183)]; t++) i[t][n(143)][n(158)] = s[n(277)] === n(170) ? n(201) : n(248);
+                    o.style.opacity = 0, o[n(143)][n(144)] = n(150), o[n(143)].transition = n(265), a[n(272)](o), b[n(272)](a), 0 <= s[n(257)] && setTimeout(() => {
+                        var t = n;
+                        p || window[t(117)] < 840 || sessionStorage[t(195)](t(214)) !== t(116) && (0 === e && (b[t(143)][t(194)] = t(186)), o[t(143)][t(175)] = 1, o[t(143)][t(144)] = "scale(1)", e === d[t(183)] - 1) && sessionStorage[t(177)]("message_bubbles_have_been_shown", t(116))
+                    }, 1e3 * s[n(257)] + 100 * e)
+                }), m[r(143)][r(246)] = s[r(277)] === r(170) ? L(r(273), .2) : L(r(164), .12), m.style.color = "dark" === s.theme ? r(201) : "black", m[r(143)][r(216)] = r(196), b[r(182)](r(211), () => {
+                    var t = r;
+                    m.style[t(194)] = t(131)
+                }), b[r(182)]("mouseleave", () => {
+                    var t = r;
+                    m.style[t(194)] = t(262)
+                }), m[r(182)](r(286), t => {
+                    var e = r;
+                    t[e(266)](), b[e(143)][e(194)] = "none"
+                }), s[r(277)] === r(170) ? r(201) : r(248));
+            g.innerHTML = r(197) + e + r(145)
+        })(), window[l(226)] && (window[l(226)][l(207)] = !0)
+    }
+    document.readyState === t(289) ? e() : window[t(182)]("load", e)
+})();
